785ebe22507a857ddf7018c31940df810a4904cf
hadeermoustafa <hadeer.moustafa@outlook.com>
2023-03-23 15:28:12 -0700

    variadic function

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,25 @@
+#include "variadic_functions.h"
+
+/*
+Define a function that takes unkown number of args
+add them all
+then return result into the screen
+*/
+
+int sum_them_all(const unsigned int n, ...)
+{
+	int sum;
+	va_list x;
+	unsigned int i;
+
+	va_start (x,n);
+	if (n == 0 )
+		return (0);
+
+	for (i = 0; i < n; i++)
+		sum += va_arg(x, int);
+       
+	va_end(x);
+	
+	return (sum);
+}
