76a9532e909dfcd62ae23c9d6eb578d484fb5630
hadeermoustafa <hadeer.moustafa@outlook.com>
2023-04-08 15:06:58 -0700

    fixing errors

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -2,13 +2,13 @@
 
 /**
  * Write a function that checks the endianness.
- *Returns: 0 if big endian, 1 if little endian
+ * Returns: 0 if big endian, 1 if little endian
  */
 
 int get_endianness(void)
 {
 	unsigned int num = 1;
-	char *endian = (char *)&num;
+	char *endian = (char *) &num;
 
 	if (*endian == 1)
 	{
