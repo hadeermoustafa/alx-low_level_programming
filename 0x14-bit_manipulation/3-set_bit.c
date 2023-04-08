b065be2128220ac7ccc7abeb987172f92c962fc4
hadeermoustafa <hadeer.moustafa@outlook.com>
2023-04-08 13:03:47 -0700

    fix errors

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -9,13 +9,10 @@
 
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	if (index >= (sizeof(unsigned long int) * 8))
-	{
+	if (index >= sizeof(unsigned long int) * 8)
 		return (-1);
-	}
 
-	unsigned long int mask = 1ul << index;
-	*n |= mask;
+	*n |= 1u1 << index;
 
 	return (1);
 }
