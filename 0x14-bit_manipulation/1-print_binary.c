9af8f5f580e49a05ee854341838bdf80576a9f88
hadeermoustafa <hadeer.moustafa@outlook.com>
2023-04-08 14:29:27 -0700

    fixing errors

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -7,7 +7,7 @@
 
 void print_binary(unsigned long int n)
 {
-	unsigned long int mask = (unsigned long int)1 << (sizeof(unsigned long int) * 8 - 1);
+	unsigned long int mask = (unsigned long int) 1 << (sizeof(unsigned long int) * 8 - 1);
 	int started = 0;
 
 	while (mask > 0)
