6747299f571919aa8ee3d33bb05d56960cc34618
hadeermoustafa <hadeer.moustafa@outlook.com>
2023-04-08 13:17:38 -0700

    fix errors

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -9,10 +9,10 @@
 
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	if (index >= sizeof(unsigned long int) * 8)  /* check for out of bounds index */
+	if (index >= sizeof(unsigned long int) * 8)
 		return -1;
 
-	*n |= (1ul << index);  /* set the bit at the index using bitwise OR */
+	*n |= (1ul << index);
 
 	return 1;
 }
