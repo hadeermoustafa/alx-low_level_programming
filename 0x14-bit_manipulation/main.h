664cac835a2814d5946abec10b07480da78bc703
hadeermoustafa <hadeer.moustafa@outlook.com>
2023-04-07 17:02:10 -0700

    fix errors

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -2,11 +2,9 @@
 #define _MAIN_
 
 #include<stdio.h>
-/**
- * Description: Bit manipulation in C
- * for ALX 
- */
+#include<stdlib.h>
 
+int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
