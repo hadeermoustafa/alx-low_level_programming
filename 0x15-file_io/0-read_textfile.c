d5d749b2f17da1f2acd053c65a6655db9c882629
hadeermoustafa <hadeer.moustafa@outlook.com>
2023-04-10 15:24:24 -0700

    write with betty

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,32 +1,30 @@
 #include "main.h"
 
 /**
- * This function takes a filename and the number of letters to read
- * from the file as input. It opens the file using the open system call, reads the
- * specified number of letters into a buffer using the read system call, and then
- * writes the contents of the buffer to standard output using the write system
- * call.
+ * The function that reads a text file 
+ * and prints it to the POSIX standard output.
  *
- * The function returns the actual number of letters read and printed on
- * success, and 0 on failure, which includes cases where the file cannot be
- * opened or read, or if the write operation fails to write the expected amount
- * of bytes to standard output. If the filename is NULL, the function also
- * returns 0.
- */
+ * it returns the actual number of letters it could read and print
+ * if the file can not be opened or read, return 0
+ *
+ * if filename is NULL return 0
+ * if write fails or does not write the expected amount of bytes, return 0
+ * */
+
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	if (filename == NULL)
-		return 0;
+		return (0);
 
 	int file = open(filename, O_RDONLY);
 	if (file == -1)
-		return 0;
+		return (0);
 
 	char *buffer = malloc(sizeof(char) * letters);
 	if (buffer == NULL)
 	{
 		close(file);
-		return 0;
+		return (0);
 	}
 
 	ssize_t letters_read = read(file, buffer, letters);
@@ -34,7 +32,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	{
 		close(file);
 		free(buffer);
-		return 0;
+		return (0);
 	}
 
 	ssize_t letters_written = write(STDOUT_FILENO, buffer, letters_read);
@@ -42,7 +40,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	free(buffer);
 
 	if (letters_written != letters_read)
-		return 0;
+		return (0);
 
-	return letters_read;
+	return (letters_read);
 }
