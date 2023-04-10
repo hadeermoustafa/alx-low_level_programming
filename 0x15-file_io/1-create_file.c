fa5f7799169fa19deec68b5369a32fb3f5f8db24
hadeermoustafa <hadeer.moustafa@outlook.com>
2023-04-10 16:47:37 -0700

    debug

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -22,7 +22,7 @@ int create_file(const char *filename, char *text_content)
 	}
 
 	file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
-	if (file == -1)
+	if (file < 0)
 	{
 		return (-1);
 	}
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -7,6 +7,7 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <string.h>
 
 int _putchar(char c);
 
