ceb508dd203608425f4b5fbd7f9de7248f0e0505
hadeermoustafa <hadeer.moustafa@outlook.com>
2023-04-10 18:25:22 -0700

    debug

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,10 +9,11 @@
  *
  * Return: 1 on success, -1 on failure
  */
+
 int create_file(const char *filename, char *text_content)
 {
-	int text_length1 = 0;
-	int text_length2;
+	int t_length = 0;
+	int tc;
 	int file_descriptor;
 
 	if (!filename)
@@ -24,11 +25,11 @@ int create_file(const char *filename, char *text_content)
 
 	if (text_content)
 	{
-		while (text_content[text_length1])
-			text_length1++;
+		while (text_content[text_length])
+			text_length++;
 
-		text_length2 = write(file_descriptor, text_content, text_length1);
-		if (text_length2 != text_length1)
+		tc = write(file_descriptor, text_content, t_length);
+		if (tc != t_length)
 			return (-1);
 	}
 
