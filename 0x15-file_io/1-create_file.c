2b53f9042fe9dd6fd7a29aa2ae19abf07b9a1a94
hadeermoustafa <hadeer.moustafa@outlook.com>
2023-04-10 18:30:54 -0700

    debug

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,38 +1,38 @@
 #include "main.h"
 
 /**
- * create_file - This function creates a file with the given name and writes the
+ * createFile - This function creates a file with the given name and writes the
  * text content to it using system calls.
  *
  * @filename: The name of the file to be created
- * @text_content: The text content to be written to the file
+ * @textContent: The text content to be written to the file
  *
  * Return: 1 on success, -1 on failure
  */
 
-int create_file(const char *filename, char *text_content)
+int createFile(const char *filename, char *textContent)
 {
-	int t_length = 0;
-	int tc;
-	int file_descriptor;
+	int lenFirst;
+	int lenSecond;
+	int fileDesc;
 
 	if (!filename)
 		return (-1);
 
-	file_descriptor = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	if (file_descriptor < 0)
+	fileDesc = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	if (fileDesc < 0)
 		return (-1);
 
-	if (text_content)
+	if (textContent)
 	{
-		while (text_content[text_length])
-			text_length++;
+		while (textContent[lenFirst])
+			lenFirst++;
 
-		tc = write(file_descriptor, text_content, t_length);
-		if (tc != t_length)
+		lenSecond = write(fileDesc, textContent, lenFirst);
+		if (lenSecond != lenFirst)
 			return (-1);
 	}
 
-	close(file_descriptor);
+	close(fileDesc);
 	return (1);
 }
