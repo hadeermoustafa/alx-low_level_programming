6268316d7ffd22e8ed0a86347ba2329bfcbc02d6
hadeermoustafa <hadeer.moustafa@outlook.com>
2023-04-10 19:38:38 -0700

    edit

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * read_textfile - reads a text file and prints it to the POSIX standard output.
+ * read_textfile - reads a text file and prints it to the POSIX standard output
  * @filename: name of the file to be read
  * @letters: number of letters to read and print
  *
