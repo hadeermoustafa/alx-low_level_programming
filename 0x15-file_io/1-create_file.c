d2a3608fbb242883c1016dd6d51dc273f57666e1
hadeermoustafa <hadeer.moustafa@outlook.com>
2023-04-10 18:06:12 -0700

    set back

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,25 +9,32 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int fl;
+	int file;
 	int text_length;
 	int bytes_written;
 
-	if (!filename)
+	if (filename == NULL)
 		return (-1);
 
-	fl = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-	if (fl < 0)
+	file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (file < 0)
 		return (-1);
 
-	if (text_content != NULL)
+	if (text_content)
 	{
-		text_length = strlen(text_content);
+<<<<<<< HEAD
+		while (text_content[text_length])
+			text_length++;
+
 		bytes_written = write(fl, text_content, text_length);
+=======
+		text_length = strlen(text_content);
+		bytes_written = write(file, text_content, text_length);
+>>>>>>> parent of b6afad3... debug
 		if (bytes_written != text_length)
 			return (-1);
 	}
 
-	close(fl);
+	close(file);
 	return (1);
 }
