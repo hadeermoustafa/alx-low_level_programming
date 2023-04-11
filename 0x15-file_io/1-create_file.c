b47c7ab5d3252bce505de472201af508515c9ffe
hadeermoustafa <hadeer.moustafa@outlook.com>
2023-04-10 18:35:10 -0700

    debug

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,38 +1,36 @@
 #include "main.h"
 
 /**
- * createFile - This function creates a file with the given name and writes the
- * text content to it using system calls.
- *
- * @filename: The name of the file to be created
- * @textContent: The text content to be written to the file
- *
- * Return: 1 on success, -1 on failure
+ * This function creates a file
+ * 
+ * Return: 1 if it succeeds, -1 if it fails.
  */
-
 int createFile(const char *filename, char *textContent)
 {
-	int lenFirst;
-	int lenSecond;
 	int fileDesc;
+	int numLetters;
+	int result;
 
 	if (!filename)
 		return (-1);
 
-	fileDesc = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	if (fileDesc < 0)
+	fileDesc = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+
+	if (fileDesc == -1)
 		return (-1);
 
-	if (textContent)
-	{
-		while (textContent[lenFirst])
-			lenFirst++;
+	if (!textContent)
+		textContent = "";
 
-		lenSecond = write(fileDesc, textContent, lenFirst);
-		if (lenSecond != lenFirst)
-			return (-1);
-	}
+	for (numLetters = 0; textContent[numLetters]; numLetters++)
+		;
+
+	result = write(fileDesc, textContent, numLetters);
+
+	if (result == -1)
+		return (-1);
 
 	close(fileDesc);
+
 	return (1);
 }
