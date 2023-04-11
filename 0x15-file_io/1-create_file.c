e0d53ed2053f8145cb2cc03d9d4a53366d4812ab
hadeermoustafa <hadeer.moustafa@outlook.com>
2023-04-10 18:45:36 -0700

    edits

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,36 +1,35 @@
 #include "main.h"
 
 /**
- * This function creates a file
- * 
- * Return: 1 if it succeeds, -1 if it fails.
+ * create_file - reads a text file and prints it to the POSIX standard output
+ *
+ * @filename: name of the file to be read
+ * @text_content: a NULL terminated string to write to the file
+ *
+ * Return: 1 on success, -1 on failure
  */
-int createFile(const char *filename, char *textContent)
+
+int create_file(const char *filename, char *text_content)
 {
-	int fileDesc;
-	int numLetters;
-	int result;
+	int word_count_one = 0, word_count_two, file_descriptor;
 
 	if (!filename)
 		return (-1);
 
-	fileDesc = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-
-	if (fileDesc == -1)
+	file_descriptor = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	if (file_descriptor < 0)
 		return (-1);
 
-	if (!textContent)
-		textContent = "";
+	if (text_content)
+	{
+		while (text_content[word_count_one])
+			word_count_one++;
 
-	for (numLetters = 0; textContent[numLetters]; numLetters++)
-		;
+		word_count_two = write(file_descriptor, text_content, word_count_one);
+		if (word_count_two != word_count_one)
+			return (-1);
+	}
 
-	result = write(fileDesc, textContent, numLetters);
-
-	if (result == -1)
-		return (-1);
-
-	close(fileDesc);
-
+	close(file_descriptor);
 	return (1);
 }
